9a70c4b3a33065d524a918eb42b49b8c0bbafe7a
richieyoum <richie.youm@koho.ca>
2024-11-29 15:10:06 -0500

    update reference to DeprecatedMixin within TimeGranularity

--- dbtsl/models/time.py
+++ dbtsl/models/time.py
@@ -1,20 +1,15 @@
 from enum import Enum
 
-from typing_extensions import override
 
-from dbtsl.models.base import DeprecatedMixin
+from dbtsl.models.base import deprecated
 
 
-class TimeGranularity(str, DeprecatedMixin, Enum):
+@deprecated(
+    "Since the introduction of custom time granularity, the `TimeGranularity` enum is deprecated. "
+    "Please just use strings to represent time grains."
+)
+class TimeGranularity(str, Enum):
     """A time granularity."""
-
-    @override
-    @classmethod
-    def _deprecation_message(cls) -> str:
-        return (
-            "Since the introduction of custom time granularity, the `TimeGranularity` enum is deprecated. "
-            "Please just use strings to represent time grains."
-        )
 
     NANOSECOND = "NANOSECOND"
     MICROSECOND = "MICROSECOND"
